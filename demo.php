dc13e3ef6605c9adf2ab97d9185150ff69e1fd7a
wangch <wangchunhui@51-dg.com>
2019-06-11 11:53:26 +0800

    init

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 86176
+ * Date: 2019/6/11
+ * Time: 11:45
+ */
+
+include "./vendor/autoload.php";
+use Tool\Encrypt\RsaEncrypt;
+RsaEncrypt::Hello();
---
+++ src/Library/Encrypt/RsaEncrypt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 86176
+ * Date: 2019/6/11
+ * Time: 11:30
+ */
+
+namespace Tool\Encrypt;
+class RsaEncrypt
+{
+    public function __construct()
+    {
+    }
+
+    public static function Hello(){
+        echo "hello";
+    }
+
+}
